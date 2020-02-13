60400f50247bcc9098adeca3d44f7e5c3abfe8a2
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2020-02-13 00:18:54 -0700

    now compiling and running with errors
    stops at input of '(x y) with a wrong type error
    brandon says might be treating x as a stand alone elemnt instead of the list '(x)

--- LA Assignments/LA1/super-duper.scm
+++ LA Assignments/LA1/super-duper.scm
@@ -9,8 +9,8 @@
 
 ;source is the list 
 ;count is the amount neet to be repeated/copied
-(define super-duper 
-    (testing-suite (source count)
+(define (super-duper source count) 
+    ；(testing-suite (source count)
          (cond
         ;checking if source is null -- if so return it
         ((null? source) source)
@@ -28,43 +28,43 @@
         (else (cons (make-list count (super-duper (car source) count)) super-duper(cdr source count)))
          )
      )
-  )
+ ； )
 
 ;testing to see if same output as provided examples in the assignment pdf
-(define testing-suite
-    (test ()
-        (display "Number based examples \n ")
-        (display "input: 123 1 \n expected output: 123 \n my output: ")
+；(define (testing-suite)
+；    (test ()
+        ;(display "Number based examples \n ")
+        (display "\n input: 123 1 \n expected output: 123 \n my output: ")
         (display (super-duper 123 1))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: 123 2 \n expected output: 123 \n my output: ")
         (display (super-duper 123 2))
-        (display " \n ")
+        (display " \n \n ")
 
-        (display "Empty List based examples \n ")
+        ;(display "Empty List based examples \n ")
         (display "input: '() 1 \n expected output: () \n my output: ")
         (display (super-duper '() 1))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: '() 2 \n expected output: () \n my output: ")
         (display (super-duper '() 2))
-        (display " \n ")
+        (display " \n \n ")
 
-        (display "Character based examples \n ")
+        ;(display "Character based examples \n ")
         (display "input: '(x) 1 \n expected output: (x) \n my output: ")
         (display (super-duper '(x) 1))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: '(x) 2 \n expected output: (x x) \n my output: ")
         (display (super-duper '(x) 2))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: '(x y) 1 \n expected output: (x y) \n my output: ")
         (display (super-duper '(x y) 1))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: '(x y) 2 \n expected output: (x x y y) \n my output: ")
         (display (super-duper '(x y) 2))
-        (display " \n ")
+        (display " \n \n ")
         (display "input: '((a b) y) 3 \n expected output: ((a a a b b b) (a a a b b b) (a a a b b b) y y y) \n my output: ")
         (display (super-duper '((a b) y) 3))
-        (display " \n ")
-    )
+        (display " \n \n ")
+    ；)
 )
 
