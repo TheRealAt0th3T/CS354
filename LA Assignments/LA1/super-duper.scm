e390b1631b124ed4c8d7cf6afa8331355e0895e2
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2020-02-13 12:15:41 -0700

    ;--;

--- LA Assignments/LA1/super-duper.scm
+++ LA Assignments/LA1/super-duper.scm
@@ -7,46 +7,47 @@
 ;car evals first element of a list
 ;cdr evals the rest of the list
 
+;function to be able to recursively deal with repeating elements
+(define (repeat sourceR countR)
+  (cond
+    ;checking if count is greater than one 
+    ((> countR 1) 
+      (cond
+        ;check if pair -- if so move down list repeating and consing 
+        ;count down the countR in order to access entire list/pair
+        ((pair? sourceR) (cons (car sourceR) (repeat sourceR (- countR 1))) sourceR)
+      )
+    sourceR
+    )
+  )
+)
+
+
 ;source is the list 
 ;count is the amount neet to be repeated/copied
 (define (super-duper source count) 
-    ；(testing-suite (source count)
-         (cond
-        ;checking if source is null -- if so return it
-        ;this if for empty lists
-        ((null? source) source)
+        ;check if number or null -- if so just simply return the source
+        (if (or (number? source)(null? source))
+          source
+          )
+         ;check count to see if list/pair or just a single element 
+         ;if single element -- just repeat it as necessary and return source
+         (if (> count 1)
+           ;if greater than one -- tis a list or pair
+           (if (list? source)
+             ;if pair -- call function to be repeating the elements and recontructing list
+             (if (pair? source)
+               (repeat (cons (super-duper (car source) count) (super-duper (cdr source) count)) count) source
+             )
+             source
+           )
+           ;source
+          )
+)
      
-        ;is it a number? if so return the number 
-        ;based of examples -- numbers get returned regaurdless of count number
-        ((number? source) source)
-     
-        ;is it a list?? repeat count times and return it
-        ;check if more than one element exists, if not, make a list that repeats count times for that first element
-        ;deals with single element lists correctly
-        ((null? (cdr source))(make-list count (car source)))
-     
-        ;ACTUALLY THE CONDITION FOR ALL LISTS OF MULTIPLE ELEMENTS!!!
-        ((list? (cdr source)) display "help"
-          ;(cons (make-list count (super-duper (car source) count)) super-duper (cdr source) count
-          ;)
-        )
-     
-     ;pair
-     ;null
-     ;symbol
-     ;number
-     
-        ;otherwise that means its a much longer list with multiple elements
-        ;repeat first element and then cons it to the next repeat
-        ;(else (cons (make-list count (super-duper (car source) count)) super-duper(cdr source count)))
-         )
-     )
- ； )
 
 ;testing to see if same output as provided examples in the assignment pdf
-；(define (testing-suite)
-；    (test ()
-        ;(display "Number based examples \n ")
+
         (display "\n input: 123 1 \n expected output: 123 \n my output: ")
         (display (super-duper 123 1))
         (display " \n \n ")
@@ -54,7 +55,6 @@
         (display (super-duper 123 2))
         (display " \n \n ")
 
-        ;(display "Empty List based examples \n ")
         (display "input: '() 1 \n expected output: () \n my output: ")
         (display (super-duper '() 1))
         (display " \n \n ")
@@ -62,7 +62,6 @@
         (display (super-duper '() 2))
         (display " \n \n ")
 
-        ;(display "Character based examples \n ")
         (display "input: '(x) 1 \n expected output: (x) \n my output: ")
         (display (super-duper '(x) 1))
         (display " \n \n ")
@@ -78,6 +77,4 @@
         (display "input: '((a b) y) 3 \n expected output: ((a a a b b b) (a a a b b b) (a a a b b b) y y y) \n my output: ")
         (display (super-duper '((a b) y) 3))
         (display " \n \n ")
-    ；)
-)
-
+    
