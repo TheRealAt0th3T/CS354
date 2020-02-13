9eaf069b4f354b7b05d9ecb61d119882fe233ff3
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2020-02-12 21:37:14 -0700

    adding super-duper function
    creating the main class super-duper -- now just to test and debug if needed

--- LA Assignments/LA1/super-duper.scm
+++ LA Assignments/LA1/super-duper.scm
@@ -2,14 +2,33 @@
 ;CS354
 ;LA1 - Scheme
 
+;notes
+;car evals first element of a list
+;cdr evals the rest of the list
+
+;source is the list 
+;count is the amount neet to be repeated/copied
 (define (super-duper source count)
     (cond
-        ;checking if null and if so - return empty list
-        ((null? source) '() )
+        ;checking if source is null -- if so return it
+        ((null? source) source)
+     
+        ;is it a number? if so return the number 
+        ;based of examples -- numbers get returned regaurdless
+        ((number? source) source)
+     
+        ;is it a list?? repeat count times and return it
+        ;check if more than one element exists, if not, make a list that repeats count times for that first element
+        ((null? (cdr source)) (make-list count (car source)))
+     
+        ;otherwise that means its a much longer list with multiple elements
+        ;repeat first element and then cons it to the next repeat
+        (else (cons (make-list count (super-duper (car source) count)) super-duper(cdr source count)))
+     )
+  )
 
-
-
-(define testing-suit
+;testing to see if same output as provided examples in the assignment pdf
+(define testing-suite
     (test ()
         (display "Number based examples \n ")
         (display "input: 123 1 \n expected output: 123 \n my output: ")
