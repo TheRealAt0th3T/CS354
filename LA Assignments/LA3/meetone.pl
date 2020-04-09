e0685709315df6e51b9ba7bdbbd0e10dc47e72d3
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2020-04-09 11:17:07 -0600

    attempt 1

--- LA Assignments/LA3/meetone.pl
+++ LA Assignments/LA3/meetone.pl
@@ -2,8 +2,10 @@
 
 :- include('data.pl').
 
-% Your code goes here.
-
+meetone(Person, slot(time(startHour, startMinute, startZone), time(endHour, endMinute, endZone))) :-
+free(Person, slot(time(sH, sM, sZ), time(eH, eM, eZ))), 
+  sH*60+sM =< endHour*60+endMinute, eH *60+eM >= startHour*60+startMinute.
+   
 main :- findall(Person,
 		meetone(Person,slot(time(8,30,am),time(8,45,am))),
 		People),
