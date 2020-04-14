ceda45ae116ef2dc7544b2622481590f239684f0
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2020-04-14 11:58:47 -0600

    version 2
    kim's help

--- LA Assignments/LA3/meetone.pl
+++ LA Assignments/LA3/meetone.pl
@@ -2,9 +2,19 @@
 
 :- include('data.pl').
 
-meetone(Person, slot(time(startHour, startMinute, startZone), time(endHour, endMinute, endZone))) :-
-free(Person, slot(time(sH, sM, sZ), time(eH, eM, eZ))), 
-  sH*60+sM =< endHour*60+endMinute, eH *60+eM >= startHour*60+startMinute.
+math(time(_,_,am),time(_,_,pm)).
+
+math(time(T1H,_,AP),time(T2H,_,AP)) :-
+  T1H<T2H.
+  
+math(time(TH,T1M,AP),time(TH,T2M,AP)) :-
+  T1M=<T2M.
+
+timein(slot(startHour, startMin), slot(endHour, endMin)) :-
+  math(startHour, startMin), math(endHour, endMin).
+
+meetone(Person, meetSlot) :-
+  free(Person, slot), timein(meetSlot, slot).
    
 main :- findall(Person,
 		meetone(Person,slot(time(8,30,am),time(8,45,am))),
